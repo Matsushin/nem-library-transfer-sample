906bf3b3c45f4e598fe1ae87297c04ca6c52aea4
matsushin11 <matsu1983@gmail.com>
2018-07-16 18:39:47 +0900

    XEMの残高を取得できるように

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Button, FormGroup, FormControl } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 import './App.css';
+import { AccountHttp, NEMLibrary, NetworkTypes, Address } from "nem-library";
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleGetAccountInfo = this.handleGetAccountInfo.bind(this);
+    this.handleChangeValue = this.handleChange.bind(this);
+
+    this.state = {
+      address: '',
+      balance: null,
+      value: null,
+    };
+  }
+
+  componentWillMount() {
+    NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+    const params = new URLSearchParams(this.props.location.search);
+    const address = params.get('address');
+    this.setBalance(address);
+  }
+
+  setBalance(address) {
+    if (address !== null) {
+      let accountHttp: AccountHttp = new AccountHttp();
+      this.setState({ address: address });
+      accountHttp.getFromAddress(new Address(address))
+        .subscribe(accountInfoWithMetaData => {
+          this.setState({ balance: accountInfoWithMetaData.balance.balance / 1000 });
+        });
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ address: e.target.value });
+  }
+
+  handleChangeValue(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleGetAccountInfo() {
+    this.props.history.push(`/?address=${this.state.address}`);
+    this.setBalance(this.state.address);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +58,31 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        NEMアドレスを入力してください。
+        <form>
+          <FormGroup>
+          <FormControl
+            type="text"
+            value={this.state.address}
+            onChange={this.handleChange}
+          />
+          </FormGroup>
+        </form>
+        <p>{ this.state.balance != null ? `${this.state.balance}XEM` : null }</p>
+        <Button bsStyle="primary" onClick={this.handleGetAccountInfo}>アカウント情報を取得する</Button>
+        <form>
+          <FormGroup>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChangeValue}
+          />
+          </FormGroup>
+        </form>
+        <Button bsStyle="primary">XEMを送金する</Button>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
